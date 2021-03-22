224e3861828dd36321d1f00885837fef6e53f357
HariharanSankar <hariharan.sankar@wipro.com>
2021-03-22 13:24:50 +0530

    1st commit

--- services/genre.js
+++ services/genre.js
@@ -1,1 +1,54 @@
-//Need to add
+const express=require('express')
+const request=require('request')
+const app=express()
+const GENREAPI="https://api.themoviedb.org/3/genre/movie/list?api_key=b7a195f9d259535f183aa711ffb7b91c&language&language=en-US";
+
+//URL to get the Genre List
+let options={
+    url:GENREAPI,
+    strictSSL:false,
+     rejectUnauthorized: false,//add when working with https sites
+     requestCert: false,//add when working with https sites
+     agent: false,
+    secureProtocol:'TLSv1_method'
+}
+
+        request.get(options,(err,response,body)=>{
+               if(err)
+               console.log(err);
+               else{
+                 let genreId=[];
+                 let movies=[];
+                 let filteredMovies=[];
+                 let genreList= 0||JSON.parse(body).genres; 
+                 let genres=req.headers['genre'].split(',');   
+                 if(genres.length!==0){
+                 genreList.forEach(element => {
+                     if(genres.includes(element.name))
+                     {
+                         genreId.push(element.id);
+                     }
+                 });
+                movieapi.results.filter(movie=>{
+                 movies.push(movie)
+                })
+                movies.filter(movie => 
+                   {
+                     genreId.forEach(id => {
+                       if(movie.genre_ids.includes(id)){
+                         filteredMovies.push(movie)
+                       }
+                     })
+                   }
+                 )
+                 console.log(`Total Movies Filtered from ${movieapi.results.length} to ${filteredMovies.length}` )
+                 response.send(filteredMovies);
+
+               }
+               else{
+                console.log("Error")
+               }
+             }
+            
+           })
+       
--- services/popularity.js
+++ services/popularity.js
@@ -1,1 +1,33 @@
 //Need to add
+
+let options1={
+    url:MOVIE_API,
+    strictSSL:false,
+     rejectUnauthorized: false,//add when working with https sites
+     requestCert: false,//add when working with https sites
+     agent: false,
+    secureProtocol:'TLSv1_method'
+}
+
+//Circuit breaker
+var CB = breaker({
+  catchError: e => 'trip',
+  handleBlockedRequest: (req, res) => res.sendStatus(500)
+})
+//Asynchronous Get Request to Fetch movie data
+app.get('/',CB, async (req,res)=>{       
+        await request.get(options1,(err,response,body)=>{
+            if(err)
+            console.log(err);
+            else{
+              let popularityRating=req.headers['popularity'];
+              let content= null ||JSON.parse(body);
+            }
+        })      
+        let results=data.results;         
+const rating=results.filter(results=>results.popularity>popularityRating)
+res.send(rating.sort())
+}
+)
+
+
