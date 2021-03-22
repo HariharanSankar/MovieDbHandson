075488e784861ecbf2d5f928f9d1d531988d3741
HariharanSankar <hariharan.sankar@wipro.com>
2021-03-22 08:51:08 +0530

    Commit 3

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const request=require('request')
 const MOVIE_API="https://api.themoviedb.org/3/movie/now_playing?api_key=b7a195f9d259535f183aa711ffb7b91c&language=en-US&page=1";
 const GENREAPI="https://api.themoviedb.org/3/genre/movie/list?api_key=b7a195f9d259535f183aa711ffb7b91c&language&language=en-US";
 const app=express()
-
-
+//URL to get The Movie List
 let options1={
     url:MOVIE_API,
     strictSSL:false,
@@ -13,7 +12,7 @@ let options1={
      agent: false,
     secureProtocol:'TLSv1_method'
 }
-
+//URL to get the Genre List
 let options2={
     url:GENREAPI,
     strictSSL:false,
@@ -23,15 +22,19 @@ let options2={
     secureProtocol:'TLSv1_method'
 
 }
+
+//Asynchronous Get Request to Fetch movie data
 app.get('/', async (req,res)=>{       
         await request.get(options1,(err,response,body)=>{
             if(err)
             console.log(err);
             else{
               let popularityRating=req.headers['popularity'];
-              let content= 0||JSON.parse(body);
+              let content= null ||JSON.parse(body);
               if(content){
+                  //Calling method to display as per user specified genre
                   getMovieWithGenre(content);
+                  //Calling method to display as per the popularity rating
                // getMoviesWithPopularity(content,popularityRating)
                   //res.send(content)
               }
@@ -52,22 +55,28 @@ app.get('/', async (req,res)=>{
                 console.log(err);
                 else{
                   let genreId=[];
+                  let movies={};
                   let genreList= 0||JSON.parse(body).genres; 
-                  let genres=req.headers['genre'];      
+                  let genres=req.headers['genre'].split(',');   
+                    console.log(genres);
                     res.json({message:"Fetched Data"})
+                  if(genres.length!==0){
                   genreList.forEach(element => {
-                      if(element.name===genres)
+                      if(genres.includes(element.name))
                       {
                           genreId.push(element.id);
-                          console.log(genreId)
-                        //  movielist.results.forEach(movies=>{
-                        //      if(movies.genre_ids.includes(genreId)){
-                        //          console.log(movies)
-                        //      }
-                        //  })
                       }
                   });
+                  movielist.forEach(movie=>{
+
+                  })
                 }
+                else{
+                  res.json({message:"No genre selected"});
+                  res.send(movielist);
+                }
+              }
+             
             })
         }
 }
