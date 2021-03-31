d67431310dafb3ac1c2d573e489fb2d2b3f0402d
HariharanSankar <hariharan.sankar@wipro.com>
2021-03-31 10:09:09 +0530

    Commit 1

--- server/services/movieService.js
+++ server/services/movieService.js
@@ -6,10 +6,10 @@ import https from 'https'
 const agent = new https.Agent({
   rejectUnauthorized: false,//add when working with https sites
 });
-    const getMovieApi =  () => {
+    const getMovieApi = async () => {
       try {
         //let popularity = req.headers.popularity
-        const resp =  axios.get(api.MOVIE_API,{ httpsAgent: agent })
+        const resp = await axios.get(api.MOVIE_API,{ httpsAgent: agent })
             .then(function (response) {
               return response.data;
             });
