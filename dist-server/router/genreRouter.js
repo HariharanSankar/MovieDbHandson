644e2b29e146fa2d2038ccc682317a89e456caf9
HariharanSankar <hariharan.sankar@wipro.com>
2021-04-01 12:44:27 +0530

    Added promise.all

--- dist-server/router/genreRouter.js
+++ dist-server/router/genreRouter.js
@@ -21,50 +21,53 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-var router = _express["default"].Router(); //Circuit breaker
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-var breaker = new _Circuitbreaker["default"](_genreService["default"]); //Get Request to Fetch movie data
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+var router = _express["default"].Router(); //Get Request to Fetch movie data
+
 
 router.get('/', function (req, res) {
-  var genreId = [];
-  var movies = [];
-  var genreList = [];
-  var filteredMovies = [];
-  var genresHeader = req.headers.genres; //              Promise.all(
-  //             [axios.get(api1, {
-  //                 httpsAgent: agent
-  //             }), axios.get(api2, {
-  //                 httpsAgent: agent
-  //             })]).then(function (responses) {
-  //             return Promise.all(responses.map(function (response) {
-  //                 return response.data;
-  //             }));
-  //         }).then(function (data) {data[0] data[1]}
+  Promise.all([_movieService["default"], _genreService["default"]]).then(function (responses) {
+    return Promise.all(responses.map(function (response) {
+      return response.data;
+    }));
+  }).then(function (data) {
+    var genreId = [];
+    var filteredMovies = [];
+    var genresHeader = req.headers['genre'].split(',');
 
-  breaker.fire().then(function (response) {
-    var genres = response.data;
+    var _ref = _toConsumableArray(data),
+        movieapi = _ref[0],
+        genreapi = _ref[1];
+
+    var movies = movieapi.results;
+    var genres = genreapi.genres;
     genres.forEach(function (element) {
       if (genresHeader.includes(element.name)) {
         genreId.push(element.id);
-        console.log(element.id);
       }
     });
+    console.log(genreId); //filtering genre ids from whole movie list by filter method
 
-    _movieService["default"].then(function (ms) {
-      ms.data.results.filter(function (movie) {
-        movies.push(movie);
+    movies.filter(function (movie) {
+      genreId.forEach(function (id) {
+        if (movie.genre_ids.includes(id)) {
+          filteredMovies.push(movie);
+        }
       });
-      movies.filter(function (movie) {
-        genreId.forEach(function (id) {
-          if (movie.genre_ids.includes(id)) {
-            filteredMovies.push(movie);
-          }
-        });
-      });
-      console.log("Movies filtered from ".concat(movies.length, " to ").concat(filteredMovies.length));
-      res.send(filteredMovies);
     });
+    console.log("Movies filtered from ".concat(movies.length, " to ").concat(filteredMovies.length));
+    res.send(filteredMovies);
   });
 });
 var _default = router;
--- server/router/genreRouter.js
+++ server/router/genreRouter.js
@@ -1,52 +1,47 @@
 import express, { response } from 'express'
-import genreService   from '../services/genreService'
-import movieService   from '../services/movieService'
+import genreService from '../services/genreService'
+import movieService from '../services/movieService'
 import CircuitBreaker from '../circuitbreaker/Circuitbreaker'
 
 
-const router=express.Router();
+const router = express.Router();
 
-//Circuit breaker
-let breaker = new CircuitBreaker(genreService);
+
 //Get Request to Fetch movie data
-router.get('/',(req,res)=>{
-    let genreId=[];
-    let movies=[];
-    let genreList=[];
-    let filteredMovies=[];
-    let genresHeader=req.headers.genres;
-    
-
-    breaker.fire().then(response=>
-        {
-            let genres=response.data;           
-          genres.forEach(element=>{
-                if(genresHeader.includes(element.name))
-                        {
-                            genreId.push(element.id);
-                            console.log(element.id)
-                        }
+router.get('/', (req, res) => {
+    Promise.all([movieService, genreService])
+        .then((responses)=> {
+            return Promise.all(responses.map((response)=> {
+                return response.data;
+            }));
+        })
+        .then((data) =>{
+            let genreId = [];
+            let filteredMovies = [];
+            let genresHeader = req.headers['genre'].split(',');
+            const [movieapi,genreapi] = [...data];
+            let movies = movieapi.results;
+            let genres = genreapi.genres;
+            genres.forEach(element => {
+                if (genresHeader.includes(element.name)) {
+                    genreId.push(element.id);
+                }
             })
-            movieService.then(ms=>                
-                {
-                    ms.data.results.filter(movie=>{
-                    movies.push(movie);                 
-                     })
-                movies.filter(movie => 
-                            {
-                                genreId.forEach(id => {
-                                    if(movie.genre_ids.includes(id)){
-                                        filteredMovies.push(movie)
-                                    }
-                                })
-                            }
-                            )                
-                            console.log(`Movies filtered from ${movies.length} to ${filteredMovies.length}`) ;           
-                            res.send(filteredMovies);
-                        })
+            console.log(genreId);
+            //filtering genre ids from whole movie list by filter method
+            movies.filter(movie => {
+                genreId.forEach(id => {
+                    if (movie.genre_ids.includes(id)) {
+                        filteredMovies.push(movie)
+                    }
+                })
             }
-        )
-})
+            )
+            console.log(`Movies filtered from ${movies.length} to ${filteredMovies.length}`);
+            res.send(filteredMovies);
+        })
 
+
+})
 
 export default router;
